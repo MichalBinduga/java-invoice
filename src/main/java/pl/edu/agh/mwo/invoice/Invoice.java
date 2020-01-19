ce94b50a700cbcbbf258c25b1d9c4596a248a02d
Michal Binduga <student12@pcoip.ki.agh.edu.pl>
2020-01-19 16:00:32 +0100

    Test Invoice - test completed

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,40 +2,53 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Map <Product, Integer> products = new HashMap<>();
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.addProduct(product,1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
+		this.products.put(product,quantity);
 		if (quantity <0 || quantity == 0) {
 			throw new IllegalArgumentException("Ilosc nie może być ujemna ani wynosić 0");
 		}
 	}
 
-	public BigDecimal getSubtotal() {
+	public BigDecimal getNetPrice() {
 		if (products == null ) {
 			return BigDecimal.ZERO;
 		}
-		return null;
+		BigDecimal sumaNetto = BigDecimal.ZERO;
+		for (Product product : this.products.keySet()){
+			Integer quantinty = this.products.get(product);
+			sumaNetto = sumaNetto.add(product.getPrice().multiply(new BigDecimal(quantinty)));
+		}
+		return sumaNetto;
 	}
 
 	public BigDecimal getTax() {
 		if (products == null) {
 			return BigDecimal.ZERO;
 		}
-		return null;
+		return this.getGrossPrice().subtract(this.getNetPrice());
 	}
 
-	public BigDecimal getTotal() {
+	public BigDecimal getGrossPrice() {
 		if (products == null) {
 			return BigDecimal.ZERO;
 		}
-		return null;
+		BigDecimal sumaBrutto = BigDecimal.ZERO;
+		for (Product product : this.products.keySet()){
+			Integer quantinty = this.products.get(product);
+			sumaBrutto = sumaBrutto.add(product.getPriceWithTax().multiply(new BigDecimal(quantinty)));
+		}
+		return sumaBrutto;
 	}
 }
